b9911f5fcff96a56e8cb1ce4c1f364edda241f3c
zyb4950045 <1014945325@qq.com>
2018-04-10 20:32:25 +0800

    合并代码

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = new int[]{6,5,2,4,3,1,0};
         System.out.println(Arrays.toString(nums));
+        
     }
 }
