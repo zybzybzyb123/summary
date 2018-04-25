f307f63fcf5039b3f71910120cd0288bce32c8a3
zyb4950045 <1014945325@qq.com>
2018-04-25 15:05:37 +0800

    添加主函数快速排序测试，添加单向扫描快速排序

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,30 @@
-package main;
+import baseSort.QuickSort;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        int[] nums = new int[]{6,5,2,4,3,1,0};
+    private static final int len = 10;
+    private static void init(int[] nums){
+        Random random = new Random();
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = random.nextInt(len);
+        }
+    }
+    private static void QuickSortTest(int[] nums){
+        System.out.println("QuickSort Test");
+        init(nums);
         System.out.println(Arrays.toString(nums));
-        
+        QuickSort.quickSort1(nums, 0, nums.length);
+        System.out.println(Arrays.toString(nums));
+        System.out.println();
+        init(nums);
+        System.out.println(Arrays.toString(nums));
+        QuickSort.quickSort2(nums, 0, nums.length);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[len];
+        QuickSortTest(nums);
     }
 }
--- src/main/baseSort/QuickSort.java
+++ src/main/baseSort/QuickSort.java
@@ -1,30 +1,33 @@
-package main.baseSort;
-
-import java.util.Arrays;
+package baseSort;
 
 public class QuickSort {
-
-    private static void quickSort1(int[] nums, int left, int right){
+    private static void swap(int[] nums, int i, int j){
+        //同意位置不交换
+        if(i == j) return;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    /**
+     * 快速排序单向扫描法
+     * @param nums
+     * @param left
+     * @param right
+     */
+    public static void quickSort1(int[] nums, int left, int right){
         if(left >= right - 1) return;
         int temp = nums[left];
-        int i = left, j = right - 1;
-        while(i < j){
-            while(i < j && nums[j] >= temp){
-                j--;
-            }
-            if(i < j){
-                nums[i++] = nums[j];
-            }
-            while(i < j && nums[i] <= temp){
+        int i = left, j = left + 1;
+        while(j < right){
+            if(nums[j] < temp){
                 i++;
+                swap(nums, i, j);
             }
-            if(i < j){
-                nums[j--] = nums[i];
-            }
+            j++;
         }
-        nums[i] = temp;
-        quickSort2(nums, left, i);
-        quickSort2(nums, i + 1, right);
+        swap(nums, left, i);
+        quickSort1(nums, left, i);
+        quickSort1(nums, i + 1, right);
     }
     /**
      * 快速排序挖洞填坑法
@@ -32,7 +35,7 @@ public class QuickSort {
      * @param left
      * @param right
      */
-    private static void quickSort2(int[] nums, int left, int right){
+    public static void quickSort2(int[] nums, int left, int right){
         if(left >= right - 1) return;
         int temp = nums[left];
         int i = left, j = right - 1;
@@ -40,23 +43,17 @@ public class QuickSort {
             while(i < j && nums[j] >= temp){
                 j--;
             }
-            if(i < j){
-                nums[i++] = nums[j];
-            }
+            nums[i] = nums[j];
             while(i < j && nums[i] <= temp){
                 i++;
             }
-            if(i < j){
-                nums[j--] = nums[i];
-            }
+            nums[j] = nums[i];
         }
         nums[i] = temp;
         quickSort2(nums, left, i);
         quickSort2(nums, i + 1, right);
     }
     public static void main(String[] args) {
-        int[] nums = new int[]{7,9,3,5,0,2,1,4};
-        quickSort2(nums, 0, nums.length);
-        System.out.println(Arrays.toString(nums));
+        System.out.println("this is quick sort");
     }
 }
